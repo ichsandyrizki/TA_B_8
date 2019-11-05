9f17da4dbca00ec6bcf1200889ff506b59846b5d
sandi.bhirama <sbhirama@gmail.com>
2019-11-05 21:47:27 +0700

    push spring project

---
+++ SI-TU/src/test/java/tugasKelompokB8/apap/SITU/SiTuApplicationTests.java
@@ -0,0 +1,13 @@
+package tugasKelompokB8.apap.SITU;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SiTuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
