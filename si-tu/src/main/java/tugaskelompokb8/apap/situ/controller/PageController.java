dd787891c94abf2c06cd33352dd463158ee27193
ichsandyrizki <ilhamrizkyakbar@gmail.com>
2019-11-06 21:50:27 +0700

    feat: add home, css js

---
+++ si-tu/src/main/java/tugaskelompokb8/apap/situ/controller/PageController.java
@@ -0,0 +1,13 @@
+package tugaskelompokb8.apap.situ.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class PageController {
+
+    @RequestMapping("/")
+    private String home(){
+        return "home";
+    }
+}
