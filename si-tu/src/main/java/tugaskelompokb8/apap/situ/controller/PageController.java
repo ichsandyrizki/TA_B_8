5b8b2401da92befdc5c136180fb2d35379c51668
Cassie <michellecassie12@gmail.com>
2019-11-07 19:02:08 +0700

    add landing page + js + css

--- si-tu/src/main/java/tugaskelompokb8/apap/situ/controller/PageController.java
+++ si-tu/src/main/java/tugaskelompokb8/apap/situ/controller/PageController.java
@@ -1,40 +1,13 @@
 package tugaskelompokb8.apap.situ.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import tugaskelompokb8.apap.situ.model.PasswordModel;
-import tugaskelompokb8.apap.situ.repository.RoleDb;
 
 @Controller
 public class PageController {
 
-	@Autowired
-	RoleDb roleDb;
-	
     @RequestMapping("/")
     private String home(){
-    	return "home";
+        return "index";
     }
-    
-    @RequestMapping("/addUser")
-    private String addUser(Model model) {
-    	model.addAttribute("listRole", roleDb.findAll());
-    	return "add-user";
-    }
-    
-    @RequestMapping("/login")
-    private String login() {
-    	return "login";
-    }
-    
-    @RequestMapping("/changePassword")
-	public String changePassword(Model model) {
-		PasswordModel changePassword = new PasswordModel();
-		model.addAttribute("changePass", changePassword);
-		model.addAttribute("message", "");
-		return "change-password";
-	}
 }
