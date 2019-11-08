c85a31afd6262820280c5b0c4335399ce807479b
Riora <satriosya@gmail.com>
2019-11-08 19:29:16 +0700

    add: html

--- si-tu/src/main/java/tugaskelompokb8/apap/situ/repository/JenisLowonganDb.java
+++ si-tu/src/main/java/tugaskelompokb8/apap/situ/repository/JenisLowonganDb.java
@@ -1,11 +1,13 @@
 package tugaskelompokb8.apap.situ.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import tugaskelompokb8.apap.situ.model.JenisLowonganModel;
 import tugaskelompokb8.apap.situ.model.JenisSuratModel;
 
 import java.util.Optional;
 
+@Repository
 public interface JenisLowonganDb extends JpaRepository<JenisLowonganModel, Long> {
     Optional<JenisLowonganModel> findById(long id);
 
