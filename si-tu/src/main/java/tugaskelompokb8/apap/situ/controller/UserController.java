5a5451382ff155287dc6eb06463572b80359c944
Sandi Bhirama <43607241+sanbhir14@users.noreply.github.com>
2019-11-08 17:52:12 +0700

    update UserController.java

--- si-tu/src/main/java/tugaskelompokb8/apap/situ/controller/UserController.java
+++ si-tu/src/main/java/tugaskelompokb8/apap/situ/controller/UserController.java
@@ -28,7 +28,7 @@ public class UserController {
 	@RequestMapping(value = "/addUser", method = RequestMethod.POST)
 	private String addUserSubmit(@ModelAttribute UserModel user) {
 		userService.addUser(user);
-		return "index";
+		return "home";
 	}
 	
 	
@@ -59,7 +59,7 @@ public class UserController {
 		}else {
 			userService.changeUser(user, changePassword.getNewPassword());
 			model.addAttribute("messages","");
-			return "index";
+			return "home";
 		}
 	}
 }
