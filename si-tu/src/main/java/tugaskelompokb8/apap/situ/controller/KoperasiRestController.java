f75f060eb47bd2dd170c7b58de07ed0e8ae988a2
Riora <satriosya@gmail.com>
2019-11-22 00:54:37 +0700

    backbone webs

--- si-tu/src/main/java/tugaskelompokb8/apap/situ/controller/KoperasiRestController.java
+++ si-tu/src/main/java/tugaskelompokb8/apap/situ/controller/KoperasiRestController.java
@@ -1,35 +1,40 @@
-//package tugaskelompokb8.apap.situ.controller;
-//
-////import apap.tutorial.gopud.model.MenuModel;
-////import apap.tutorial.gopud.model.RestoranModel;
-////import apap.tutorial.gopud.rest.RestoranDetail;
-////import apap.tutorial.gopud.service.RestoranRestService;
-////import apap.tutorial.gopud.service.RestoranService;
-//
-//import tugaskelompokb8.apap.situ.model.*;
-//import tugaskelompokb8.apap.situ.repository.*;
-//import tugaskelompokb8.apap.situ.rest.*;
-//import tugaskelompokb8.apap.situ.service.*;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.beans.factory.annotation.Qualifier;
-//import org.springframework.http.HttpStatus;
-//import org.springframework.http.ResponseEntity;
-//import org.springframework.ui.Model;
-//import org.springframework.web.bind.annotation.*;
-//import org.springframework.web.server.ResponseStatusException;
-//import reactor.core.publisher.Mono;
-//import org.springframework.http.HttpEntity;
-//import org.springframework.http.HttpMethod;
-//import org.springframework.web.client.RestTemplate;
-//
-//import java.util.List;
-//import java.util.NoSuchElementException;
-//import javax.validation.Valid;
-//
-//@RestController
-//@RequestMapping("/sikoperasi/")
-//public class KoperasiRestController {
-//
-//    @Autowired
-//    WebService web
-//}
+package tugaskelompokb8.apap.situ.controller;
+
+//import apap.tutorial.gopud.model.MenuModel;
+//import apap.tutorial.gopud.model.RestoranModel;
+//import apap.tutorial.gopud.rest.RestoranDetail;
+//import apap.tutorial.gopud.service.RestoranRestService;
+//import apap.tutorial.gopud.service.RestoranService;
+
+import tugaskelompokb8.apap.situ.model.*;
+import tugaskelompokb8.apap.situ.repository.*;
+import tugaskelompokb8.apap.situ.rest.*;
+import tugaskelompokb8.apap.situ.service.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+import reactor.core.publisher.Mono;
+
+import java.util.List;
+import java.util.NoSuchElementException;
+import javax.validation.Valid;
+
+@RestController
+@RequestMapping("/sikop")
+public class KoperasiRestController {
+
+    @Autowired
+    private PengajuanPinjamanService pengajuanPinjamanService;
+
+    @PostMapping(value = "/addPengajuan")
+    private Mono<String> postStatus(){
+        return pengajuanPinjamanService.postPinjamanKoperasi();
+    }
+
+
+
+}
--- si-tu/src/main/java/tugaskelompokb8/apap/situ/rest/Setting.java
+++ si-tu/src/main/java/tugaskelompokb8/apap/situ/rest/Setting.java
@@ -3,4 +3,5 @@ package tugaskelompokb8.apap.situ.rest;
 public class Setting {
 	final public static String userURL = "http://sivitas.herokuapp.com/api/employees/";
 	final public static String pengajuanURL = "http://ba171c4e-87fb-49b2-a169-76a6aa65e9eb.mock.pstmn.io/rest/situ/pengajuanSurat";
+	final public static String koperasiURL = "http://ba171c4e-87fb-49b2-a169-76a6aa65e9eb.mock.pstmn.io/rest/situ/blablabla";
 }
