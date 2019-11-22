ac062eaee2c072972a223450898e9c344a0dec33
Angelina Condro <angelina.condro@ymail.com>
2019-11-22 18:24:57 +0700

    feat-12-done

--- si-tu/src/main/java/tugaskelompokb8/apap/situ/service/LowonganRestService.java
+++ si-tu/src/main/java/tugaskelompokb8/apap/situ/service/LowonganRestService.java
@@ -2,10 +2,12 @@ package tugaskelompokb8.apap.situ.service;
 
 import reactor.core.publisher.Mono;
 import tugaskelompokb8.apap.situ.model.LowonganModel;
+import tugaskelompokb8.apap.situ.rest.BaseResponse;
+import tugaskelompokb8.apap.situ.rest.BaseRest;
 import tugaskelompokb8.apap.situ.rest.UserPerpusDetail;
 
 public interface LowonganRestService {
 //    LowonganDetail getLowonganById(Long id);
     LowonganModel createLowongan(LowonganModel lowongan);
-    Mono<UserPerpusDetail> getUser(String role);
+    Mono<BaseResponse> getAllUser();
 }
