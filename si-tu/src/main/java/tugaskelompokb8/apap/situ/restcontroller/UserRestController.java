6bc8a3fd5835fc6f0546476e89bba5843a549258
Cassie <michellecassie12@gmail.com>
2019-11-22 11:56:29 +0700

    delete UserSivitasList

--- si-tu/src/main/java/tugaskelompokb8/apap/situ/restcontroller/UserRestController.java
+++ si-tu/src/main/java/tugaskelompokb8/apap/situ/restcontroller/UserRestController.java
@@ -23,5 +23,5 @@ public class UserRestController {
 	@Qualifier("userRestServiceImpl")
 	@Autowired
 	UserRestService userRestService;
-	
+
 }
--- si-tu/src/main/java/tugaskelompokb8/apap/situ/service/UserRestService.java
+++ si-tu/src/main/java/tugaskelompokb8/apap/situ/service/UserRestService.java
@@ -2,10 +2,7 @@ package tugaskelompokb8.apap.situ.service;
 
 import reactor.core.publisher.Mono;
 import tugaskelompokb8.apap.situ.rest.BaseRest;
-import tugaskelompokb8.apap.situ.rest.UserSivitasList;
 import tugaskelompokb8.apap.situ.rest.UserSivitasModel;
-
-import java.util.List;
 
 public interface UserRestService {
     Mono<BaseRest> registerUser (UserSivitasModel userSivitasModel);
